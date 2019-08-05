f8c6d01e959fc8b12d613a83db486de854062e2c
oshynkare <Qwerty1985Kh>
2019-08-05 17:31:04 +0300

    Project setup

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+
+import styles from './AppStyles'
+
+function App() {
+  return (
+    <div css={styles.app}>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App/AppStyles.js
@@ -0,0 +1,9 @@
+const appStyles = {
+  app: {
+    background: 'red',
+    margin: '0 auto',
+    textAlign: 'center',
+  }
+};
+
+export default appStyles;
