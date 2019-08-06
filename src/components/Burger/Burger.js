7599a939b896a8f8a3c84c267742189679c043d7
oshynkare <Qwerty1985Kh>
2019-08-06 15:40:45 +0300

    add possibility to add ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,12 +6,18 @@ import { BurgerIngredients } from "../BurgerIngridients/BurgerIngredients";
 import burgerContainerStyles from "./BurgerContainerStyles";
 
 export const Burger = ( props ) => {
+
+  const ingredientsTypes = Object.keys(props.ingredients)
+      .map(ingredient => {
+        return [...Array(props.ingredients[ingredient])].map((_, i) => {
+          return <BurgerIngredients type={ingredient} key={ingredient + i}/>
+        })
+      });
+
   return (
     <div css={burgerContainerStyles}>
       <BurgerIngredients type='bread-top'/>
-      <BurgerIngredients type='cheese'/>
-      <BurgerIngredients type='bacon'/>
-      <BurgerIngredients type='meat'/>
+        {ingredientsTypes}
       <BurgerIngredients type='bread-bottom'/>
     </div>
   )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,20 @@ import React, {Component} from 'react';
 import { Burger } from "../../components/Burger/Burger";
 
 export class BurgerBuilder extends Component {
+
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 2,
+      cheese: 2,
+      meat: 1
+    }
+  };
+
   render() {
     return (
         <>
-          <Burger />
+          <Burger ingredients={this.state.ingredients}/>
           <div>Burger Builder Controls</div>
         </>
     )
